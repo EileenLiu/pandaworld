ba8d4453a7b46ec45330ac5b6e84401f06994e66
Eileen Liu <el544@cornell.edu>
2012-10-11 14:25:35 -0400

    Changes to FaultInjector, call node methods instead of editing fields in node directly

--- student/FaultInjector.java
+++ student/FaultInjector.java
@@ -10,33 +10,52 @@ public class FaultInjector {
 		//Node[] arr = n.toArray();
 		//int i = (int)(Math.random()*arr.length);
 		int faultType = (int)(Math.random()*5);
+		boolean go = true;
+		while(go)
+		{
+			go = false;
 		switch(faultType){
 		case 0: //the node is removed. if its parent node needs a replacement node, one of its children of the right kind is used. The child to be used is randomly selected. Thus rule nodes are simply removed, but binary operation nodes would be replaced with either their left or right child
-			if(n.parent instanceof BinaryOp)
+			//find selected
+			Node selected;
+			selected.remove();
+			/*if(n.parent instanceof BinaryOp)
 			{
 				//find selected
 				Node selected;
 				p.value = selected.value;
 			}
 			else
-				n.parent.children = null;
+				n.parent.children = null;*/
 			break;
 		case 1: //order of two children of the node is switched. for ex. allows swapping of positions of two rules
-			Node left = n.left;
+			if(n.hasChildren())
+			{
+			n.swapChildren();
+			}
+			else
+				go = true;
+			/*Node left = n.left;
 			n.left = right;
-			n.right = left;
+			n.right = left;*/
 			break;
 		case 2: //the node and its children are replaced with a copy of another randomly selected node of the right kind, found somewhere in the rule set. the entire AST subtree rooted at the selected node is copied
 			//find selected
 			Node selected;
+			if(selected!=null)
+			{
 			n.set(selected.copy());
+			}
+			else
+				go = true;
 			break;
 		case 3:// the node is replaced with a randomly chosen node of the same kind( for example, replacing attack with eat, or + with *) but its children remain the same. Literal integer constants are adjusted up or down with the value of java.lang.Integer.MAX_VALUE/r.nextInt(), where legal, and where r is a java.util.random obj
 			Node selected;
-			n.value = selected.value;
+			n.setValue(selected.getValue);
 			break;
 		case 4: // a newly created node is inserted as the parent of the node, taking its place in the tree. if the newly created node has more than one child, the children that are not the original node are copies of randomly chosen nodes of the right kind from the whole rule set
 			break;
+		}
 		}
 		/*		for(int i=0; i<ORIGINAL.length; i++)
 		{
