39efb31b3f27d7df7d5e1f0c5cd0da27f5da2258
Eileen Liu <contact.eileenliu@gmail.com>
2012-10-16 01:59:05 -0400

    FaultInjector: fault injection type 5 done
    Signed-off-by: Eileen Liu <contact.eileenliu@gmail.com>

--- student/FaultInjector.java
+++ student/FaultInjector.java
@@ -20,16 +20,20 @@ public class FaultInjector {
         LinkedList<Integer> faultType = new LinkedList();
         faultType.addAll(Arrays.asList(1,2,3,4,5));
         int i;      
-        while (!faultType.isEmpty()){
-            i = (int)(Math.random()*faultType.size());
+        while (!faultType.isEmpty()){ //while there are still fault types to try...
+            i = (int)(Math.random()*faultType.size()); 
             switch (faultType.get(i)) {
                 case 1: //the node is removed. if its parent node needs a replacement node, one of its children of the right kind is used. The child to be used is randomly selected. Thus rule nodes are simply removed, but binary operation nodes would be replaced with either their left or right child
                     if(n.getParent().deleteChild(n))
+                    {
+                        n.setMutationType(1);
                         return root;
+                    }
                     break;
                 case 2: //order of two children of the node is switched. for ex. allows swapping of positions of two rules
                     if (n.numChildren() >= 2) {
                         n.swapChildren();
+                        n.setMutationType(2);
                         return root;
                     }
                     break;
@@ -44,47 +48,57 @@ public class FaultInjector {
                     Node selected = randomNode(root, n.getClass());
                     if (selected != null) {
                         n.set(selected.copy());
+                        n.setMutationType(3);
                         return root;
                     }
                     break;
                 case 4:// the node is replaced with a randomly chosen node of the same kind( for example, replacing attack with eat, or + with *) but its children remain the same. Literal integer constants are adjusted up or down with the value of java.lang.Integer.MAX_VALUE/r.nextInt(), where legal, and where r is a java.util.random obj
                     //cannot: update, condition, node, program, rule				
                     if (n.randomize()) {
+                        n.setMutationType(4);
                         return root;
                     }
                     break;
                 case 5: // a newly created node is inserted as the parent of the node, taking its place in the tree. if the newly created node has more than one child, the children that are not the original node are copies of randomly chosen nodes of the right kind from the whole rule set
                     // TODO CASE 4
                     Node parent = n.getParent();
+                    n.setMutationType(5); //assume the mutation is applicable for now
                     if (parent instanceof Access) {
+                        BinaryArithmeticOperator bao = new BinaryArithmeticOperator((Expression)n, (Expression)(randomNode(root, Expression.class).copy()));
+                        parent.replaceChild(n, bao);
+                        return root;
                     } 
                     else if (parent instanceof BinaryArithmeticOperator) {
                         BinaryArithmeticOperator bao = new BinaryArithmeticOperator((Expression)n, (Expression)(randomNode(root, Expression.class).copy()));
                         parent.replaceChild(n, bao);
+                        return root;
                     } 
                     else if (parent instanceof BinaryBooleanOperator) {
+                        BinaryBooleanOperator boo = new BinaryBooleanOperator((Condition)n, (Condition)(randomNode(root, Condition.class).copy()));
+                        parent.replaceChild(n, boo);
+                        //don't create new BinaryRelation
                     } 
                     else if (parent instanceof BinaryRelation) {
                         BinaryArithmeticOperator bao = new BinaryArithmeticOperator((Expression)n, (Expression)(randomNode(root, Expression.class).copy()));
                         parent.replaceChild(n, bao);
-                        //replace
+                        return root;
                     } 
-                    else if (parent instanceof Condition) {
-                    } 
-                    else if (parent instanceof Program) {
-                    } 
-                    else if (parent instanceof Rule) {
+                    else if (parent instanceof Rule && n instanceof Condition) {
+                        BinaryBooleanOperator boo = new BinaryBooleanOperator((Condition)n, (Condition)(randomNode(root, Condition.class).copy()));
+                        parent.replaceChild(n, boo);
                     } 
                     else if (parent instanceof Tag) {
                         BinaryArithmeticOperator bao = new BinaryArithmeticOperator((Expression)n, (Expression)(randomNode(root, Expression.class).copy()));
                         parent.replaceChild(n, bao);
-                        //replace
+                        return root;
                     }
+                    else //the mutation is not applicable
+                        n.setMutationType(0); //reset--do not store mutation type since mutation was not applied
                     break;
             }
-            faultType.remove(i);
+            faultType.remove(i); //deletes the last fault type from the list of possible fault types because it was already tried (and failed)
         }
-        return null;
+        return null; //none of the possible fault types worked!
     }
 
     
