888d1ca67e7843aae180d42c1e8ea049cb2a4998
Harrison W. Houghton '16 <haro@haro-Satellite-P740D.(none)>
2012-10-28 16:43:06 -0400

    merged world to world Signed-off-by: Harrison W. Houghton '16 <haro@haro-Satellite-P740D.(none)>

--- student/world/World.java
+++ student/world/World.java
@@ -4,15 +4,25 @@
  */
 package student.world;
 
+import student.grid.ArrayHexGrid;
+import student.grid.Entity;
+import student.grid.HexGrid;
+
 /**
  *
  * @author Panda
  */
 public class World {
 
-    public String getStatus()
-    {
+    public String getStatus() {
         //TODO: get status
         return "";
+    }
+    HexGrid<Entity> world;
+    int siz;
+
+    public World(int _siz) {
+        siz = _siz;
+        world = new ArrayHexGrid<Entity>(siz);
     }
 }
