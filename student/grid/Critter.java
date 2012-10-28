0abfa05475a3e3ab1c7629cd15671c71ac90553b
Harrison W. Houghton '16 <haro@haro-Satellite-P740D.(none)>
2012-10-28 19:05:35 -0400

    Added properties for Entities Signed-off-by: Harrison W. Houghton '16 <haro@haro-Satellite-P740D.(none)>

--- student/grid/Critter.java
+++ student/grid/Critter.java
@@ -10,141 +10,197 @@ import student.grid.HexGrid.HexDir;
 import static student.grid.HexGrid.HexDir.*;
 import student.grid.HexGrid.Reference;
 import student.world.World;
+import sun.misc.Unsafe;
 
 /**
  *
  * @author haro
  */
 public class Critter extends Entity {
-    
+
     private World wor;
     private Reference<Set<Entity>> pos;
     private HexDir dir;
-    private int energy;
-    private int size, mem[];
+    private int mem[];
     private boolean acted;
-    
+
     public Critter(World _wor, Reference<Set<Entity>> _pos, int memsiz) {
-        wor = _wor; pos = _pos;
+        wor = _wor;
+        pos = _pos;
         mem = new int[memsiz];
     }
+
+    @IsProperty
+    public HexDir direction() {
+        return dir;
+    }
+
+    @IsProperty
+    public int size() {
+        return mem[3];
+    }
     
+    @IsProperty
+    public int memsize() {
+        return mem.length;
+    }
+    
+    @IsProperty
+    public int defense() {
+        return mem[1];
+    }
+    
+    @IsProperty
+    public int offense() {
+        return mem[2];
+    }
+    
+    @IsProperty
+    public int ruleCount() {
+        return mem[5];
+    }
+    
+    @IsProperty
+    public int log() {
+        return mem[6];
+    }
+    
+    @IsProperty
+    public int tag() {
+        return mem[7];
+    }
+    
+    @IsProperty
+    public int posture() {
+        return mem[8];
+    }
+    
+    @IsProperty
+    public int []memory() {
+        int rest[] = new int[mem.length - 9];
+        System.arraycopy(mem, 9, rest, 0, mem.length-9);
+        return rest;
+    }
+
     @Override
+    @IsProperty("energy")
     public int read() {
-        return energy;
+        return mem[4];
     }
 
     @Override
     public void timeStep() {
-        if(!acted)
+        if (!acted) {
             _wait();
+        }
         acted = false;
-        if(energy < 0)
+        if (mem[4] < 0)
             ;//die
     }
-    
+
     @IsAction("wait")
     public void _wait() {
-        energy -= size;
+        mem[4] -= mem[3];
         acted = true;
     }
-    
+
     @IsAction
     public void forward() {
-        energy -= size * MOVE_COST;
+        mem[4] -= mem[3] * MOVE_COST;
         pos.contents().remove(this);
         pos = pos.adj(dir);
         pos.contents().add(this);
         acted = true;
     }
-    
+
     @IsAction
     public void backward() {
-        energy -= size * MOVE_COST;
+        mem[4] -= mem[3] * MOVE_COST;
         pos.contents().remove(this);
         pos = pos.lin(-1, dir);
         pos.contents().add(this);
         acted = true;
     }
-    
+
     @IsAction
     public void left() {
-        energy -= size;
-        dir = dir == N?NW:
-              dir == NW?SW:
-              dir == SW?S:
-              dir == S?SE:
-              dir == SE?NE:
-              dir == NE?N:null;
+        mem[4] -= mem[3];
+        dir = dir == N ? NW
+                : dir == NW ? SW
+                : dir == SW ? S
+                : dir == S ? SE
+                : dir == SE ? NE
+                : dir == NE ? N : null;
         acted = true;
     }
-    
+
     @IsAction
     public void right() {
-        energy -= size;
-        dir = dir == N?NE:
-              dir == NE?SE:
-              dir == SE?S:
-              dir == S?SW:
-              dir == SW?NW:
-              dir == NW?N:null;
+        mem[4] -= mem[3];
+        dir = dir == N ? NE
+                : dir == NE ? SE
+                : dir == SE ? S
+                : dir == S ? SW
+                : dir == SW ? NW
+                : dir == NW ? N : null;
         acted = true;
     }
-    
+
     @IsAction
     public void eat() {
-        energy -= size;
-        for(Entity e : pos.contents())
-            if(e instanceof Food) {
-                int ene = ((Food)e).energy;
-                System.out.println("Ate "+ene+" units of energy");
-                energy += ene;
-                return;
-            }
-        acted = true;
-    }
-    
-    @IsAction
-    public void attack() {
-        energy -= size * ATTACK_COST;
-        Set<Entity> ahead = pos.adj(dir).contents();
-        for(Entity e : ahead) {
-            if(e instanceof Critter) {
-                Critter c = (Critter)e;
-                double damage = BASE_DAMAGE * size
-                              * lgs(DAMAGE_INC * (size * mem[2] - c.size * c.mem[1]));
-                c.energy -= (int) damage;
+        mem[4] -= mem[3];
+        for (Entity e : pos.contents()) {
+            if (e instanceof Food) {
+                int ene = ((Food) e).energy;
+                System.out.println("Ate " + ene + " units of mem[4]");
+                mem[4] += ene;
                 return;
             }
         }
         acted = true;
     }
-    
+
+    @IsAction
+    public void attack() {
+        mem[4] -= mem[3] * ATTACK_COST;
+        Set<Entity> ahead = pos.adj(dir).contents();
+        for (Entity e : ahead) {
+            if (e instanceof Critter) {
+                Critter c = (Critter) e;
+                double damage = BASE_DAMAGE * mem[3]
+                        * lgs(DAMAGE_INC * (mem[3] * mem[2] - c.mem[3] * c.mem[1]));
+                c.mem[4] -= (int) damage;
+                return;
+            }
+        }
+        acted = true;
+    }
+
     @IsAction
     public void tag(int t) {
-        energy -= size;
+        mem[4] -= mem[3];
         Set<Entity> ahead = pos.adj(dir).contents();
-        for(Entity e : ahead) {
-            if(e instanceof Critter) {
-                Critter c = (Critter)e;
+        for (Entity e : ahead) {
+            if (e instanceof Critter) {
+                Critter c = (Critter) e;
                 c.mem[7] = t;
             }
         }
         acted = true;
     }
-    
+
     @IsAction
     public void grow() {
-        energy -= size * complexity() * GROW_COST;
-        size++;
+        mem[4] -= mem[3] * complexity() * GROW_COST;
+        mem[3]++;
         acted = true;
     }
-    
+
     private static double lgs(double x) {
-        return 1/(1+Math.exp(-x));
+        return 1 / (1 + Math.exp(-x));
     }
-    
+
     private int complexity() {
-        return /*rules * RULE_COST +*/ (mem[1] + mem[2])*ABILITY_COST;
+        return /*rules * RULE_COST +*/ (mem[1] + mem[2]) * ABILITY_COST;
     }
 }
--- student/grid/Entity.java
+++ student/grid/Entity.java
@@ -8,9 +8,12 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * The model class for an entity (rock/critter/plant)
@@ -28,11 +31,25 @@ public abstract class Entity {
         public abstract Object exec(Object...p);
         public Class<?> parType[];
     }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.METHOD)
+    public @interface IsProperty {
+        public String value() default "";
+    }
     
-    private final Set<Action> actions; {
+    public static abstract class Property {
+        public String name;
+        public Class<?> type;
+        public abstract Object value();
+    }
+    
+    private final Set<Action> actions; 
+    private final Set<Property> properties; {
         Class<? extends Entity> self = getClass();
         Method meths[] = self.getMethods();
         Set<Action> acts = new HashSet<Action>();
+        Set<Property> props = new HashSet<Property>();
         for(Method m : meths) {
             if(m.isAnnotationPresent(IsAction.class)) {
                 IsAction ia = m.getAnnotation(IsAction.class);
@@ -61,12 +78,42 @@ public abstract class Entity {
                 }.set(name, m, this);
                 acts.add(a);
             }
+            else if(m.isAnnotationPresent(IsProperty.class)) {
+                IsProperty ip = m.getAnnotation(IsProperty.class);
+                String name = ip.value();
+                if("".equals(name))
+                    name = m.getName();
+                Property p = new Property() {
+                    Entity e; Method m;
+                    public Property set(String _name, Method _m, Entity _e) {
+                        e = _e; m = _m;
+                        name = _name;
+                        type = m.getReturnType();
+                        return this;
+                    }
+                    @Override
+                    public Object value() {
+                        try {
+                            return m.invoke(e);
+                        } catch (RuntimeException re) {
+                            throw re;
+                        } catch (Exception ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
+                }.set(name, m, this);
+                props.add(p);
+            }
         }
         actions = acts;
-    }
+        properties = props;
+    }   
     
     public Set<Action> actions() {
         return actions;
+    }
+    public Set<Property> properties() {
+        return properties;
     }
     
     public abstract int read();
