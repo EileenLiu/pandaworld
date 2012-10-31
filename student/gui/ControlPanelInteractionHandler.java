56e744ad8ac098edd23493f2bc0ac8fdb4cb3968
Eileen Liu <contact.eileenliu@gmail.com>
2012-10-30 23:47:50 -0400

    added new class ControlPanelInteractionHandler Signed-off-by: Eileen Liu <contact.eileenliu@gmail.com>

---
+++ student/gui/ControlPanelInteractionHandler.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student.gui;
+
+import student.world.World;
+
+/**
+ *
+ * @author Eileen Liu <el544@cornell.edu>
+ */
+public class ControlPanelInteractionHandler {
+    private World model;
+    private WorldFrame view;
+    public ControlPanelInteractionHandler(final World _model, final WorldFrame _view) {
+        model = _model;
+        view = _view;
+ //       view.worldDisplay.controls.random
+        //view.worldDisplay.controls.runButton
+   //                             view.worldDisplay.controls.random
+    }
+}
--- student/world/World.java
+++ student/world/World.java
@@ -21,7 +21,8 @@ public class World {
     private static final int DEFAULT_COLS = 6;
     HexGrid<Tile> grid;
     private int timesteps = 0;
-
+    private boolean RUNNING = false;
+    private boolean WAIT = true; //if false, random action
     public World() {
         this(DEFAULT_ROWS, DEFAULT_COLS);
     }
