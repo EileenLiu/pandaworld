23e3b3fc51f56fbdc0cc221bc369c17824315a96
Harrison W. Houghton '16 <haro@haro-Satellite-P740D.(none)>
2012-10-31 23:54:47 -0400

    Removed debugging shtuff Signed-off-by: Harrison W. Houghton '16 <haro@haro-Satellite-P740D.(none)>

--- student/gui/GridPanel.java
+++ student/gui/GridPanel.java
@@ -258,9 +258,11 @@ public class GridPanel extends JPanel implements Scrollable{
                         drawHexagon(bbx.x, bbx.y, r, c, hexsize, gp, i);
                     }
                 }
+                /*/
                 gp.setColor(Color.RED);
                 String s = "("+r+","+c+")";
                 gp.drawChars(s.toCharArray(), 0, s.length(), bbx.x+20, bbx.y+20);
+                /*/
             }
         }
     }
