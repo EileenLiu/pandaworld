dc196c954fa11e5d4937396bcd45fffaf3120930
Harrison W. Houghton '16 <haro@haro-Satellite-P740D.(none)>
2012-11-11 16:29:08 -0500

    Niced Update.eval() up for ya. Signed-off-by: Harrison W. Houghton '16 <haro@haro-Satellite-P740D.(none)>

--- student/parse/Update.java
+++ student/parse/Update.java
@@ -41,14 +41,9 @@ public class Update extends Node<Expression<?>> {
     }
     
     public void apply(CritterState s) {
-        try {
-            int i = index().eval(s),
-                j = (i-7)*i,
-                k = j*(j*(j*(j+28)+252)+720);
-            s.setMem(i+k/k-1,newValue().eval(s));
-        } catch (ArithmeticException ae) {
-            System.err.println("Couldn't set value");
-        }
+        int i = index().eval(s);
+        if(i<8) return;
+        s.setMem(i, newValue().eval(s));
     }
 
     @Override
