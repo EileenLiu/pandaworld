ca53d39d17fcc207edde85e666ddeccac3ca449c
Eileen Liu <contact.eileenliu@gmail.com>
2012-11-28 02:02:03 -0500

    Filled in methods of RingBuffer cept BlockingQueue Signed-off-by: Eileen Liu <contact.eileenliu@gmail.com>

--- student/concurrency/RingBuffer.java
+++ student/concurrency/RingBuffer.java
@@ -5,53 +5,48 @@
 package student.concurrency;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
 
 /**
  *
  * @author Eileen Liu <el544@cornell.edu>
  */
-public class RingBuffer<T> implements BlockingQueue<T>{
-    private T[] queue; 
+public class RingBuffer<T> implements BlockingQueue<T> {
+
+    private T[] queue;
     private int headIndex = -1;
     private int tailIndex = -1;
 
     public RingBuffer(int capacity) {
-        queue = (T [])(Array.newInstance(Object.class, capacity));
+        queue = (T[]) (Array.newInstance(Object.class, capacity));
         headIndex = 0;
         tailIndex = 0;
-    }
-    private void incrementHeadIndex(){
-        headIndex = headIndex%queue.length + 1;
-    }
-    private void incrementTailIndex() {
-        tailIndex = tailIndex % queue.length + 1;
     }
 
     @Override
     public boolean add(T e) {
-        if ((tailIndex-headIndex)<(queue.length)) {
-        queue[tailIndex] = e;
-        tailIndex++;
-        return true;
+        if ((tailIndex - headIndex) < (queue.length)) {
+            queue[tailIndex % queue.length] = e;
+            tailIndex++;
+            return true;
         }
         return false;
     }
-    
+
     @Override
     public boolean remove(Object o) {
-        if(!(tailIndex==headIndex))
-        {
+        if (!(tailIndex == headIndex)) {
             Iterator<T> iter = this.iterator();
-            while(iter.hasNext())
-            {
+            while (iter.hasNext()) {
                 T next = iter.next();
-                if(next.equals(o))
-                {
+                if (o.equals(next)) {
                     iter.remove();
                     return true;
                 }
@@ -59,7 +54,7 @@ public class RingBuffer<T> implements BlockingQueue<T>{
         }
         return false;
     }
-    
+
     @Override
     public boolean offer(T e) {
         throw new UnsupportedOperationException("Not supported yet.");
@@ -67,7 +62,6 @@ public class RingBuffer<T> implements BlockingQueue<T>{
 
     @Override
     public void put(T e) throws InterruptedException {
-        
     }
 
     @Override
@@ -87,14 +81,19 @@ public class RingBuffer<T> implements BlockingQueue<T>{
 
     @Override
     public int remainingCapacity() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return (queue.length - (tailIndex - headIndex));
     }
-
-    
 
     @Override
     public boolean contains(Object o) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Iterator<T> iter = this.iterator();
+        while (iter.hasNext()) {
+            T next = iter.next();
+            if (o.equals(next)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
@@ -109,36 +108,54 @@ public class RingBuffer<T> implements BlockingQueue<T>{
 
     @Override
     public T remove() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        T removed = this.poll();
+        if (removed == null) {
+            throw new NoSuchElementException();
+        }
+        return removed;
     }
 
     @Override
     public T poll() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try{
+        T removed = queue[headIndex];
+        queue[headIndex] = null;
+        headIndex++;
+        return removed;
+        }
+        catch(ArrayIndexOutOfBoundsException a)
+        {
+           return null; 
+        }
     }
+
     @Override
     public T element() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        T elem = this.peek();
+        if (elem == null) {
+            throw new NoSuchElementException();
+        }
+        return elem;
     }
 
     @Override
     public T peek() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return isEmpty() ? null : queue[headIndex];
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return (tailIndex - headIndex);
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return (queue == null || queue.length == 0 || headIndex == tailIndex);
     }
 
     @Override
     public Iterator<T> iterator() {
-        return new RingIterator();        
+        return new RingIterator();
     }
 
     @Override
@@ -149,18 +166,51 @@ public class RingBuffer<T> implements BlockingQueue<T>{
         if (getClass() != obj.getClass()) {
             return false;
         }
-        //////////////////////
-        return true;
+        Iterator<T> objiter = ((RingBuffer)obj).iterator();
+        Iterator<T> iter = this.iterator();
+        boolean match = true;
+        while(iter.hasNext()&&objiter.hasNext())
+        {
+            match = match&&(iter.next().equals(objiter.next()));
+        }
+        return (match&&!iter.hasNext()&&!objiter.hasNext());
     }
-    
-    
-    ////////////===========UNSUPPORTED METHODS============//////////////
-    
-    
-        @Override
+
+    @Override
     public Object[] toArray() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return Arrays.copyOfRange(queue, headIndex, tailIndex);
     }
+
+    @Override
+    public void clear() {
+        queue = (T[]) (Array.newInstance(Object.class, queue.length));
+        headIndex = 0;
+        tailIndex = 0;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        boolean wasSuccessful = true;
+        if (((tailIndex + c.size() - headIndex) > (queue.length))) {
+            throw new IllegalStateException();
+        }
+        for (T elem : c) {
+            wasSuccessful = (wasSuccessful && add(elem));
+        }
+        return wasSuccessful;
+    }
+
+    public boolean addAll(T... c) {
+        boolean wasSuccessful = true;
+        if (((tailIndex + c.length - headIndex) > (queue.length))) {
+            throw new IllegalStateException();
+        }
+        for (T elem : c) {
+            wasSuccessful = (wasSuccessful && add(elem));
+        }
+        return wasSuccessful;
+    }
+    ////////////===========UNSUPPORTED METHODS============//////////////
 
     @Override
     public <T> T[] toArray(T[] a) {
@@ -173,11 +223,6 @@ public class RingBuffer<T> implements BlockingQueue<T>{
     }
 
     @Override
-    public boolean addAll(Collection<? extends T> c) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
     public boolean removeAll(Collection<?> c) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
@@ -186,27 +231,26 @@ public class RingBuffer<T> implements BlockingQueue<T>{
     public boolean retainAll(Collection<?> c) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-    
     //////////////////////////////////////////
-    
+
     private class RingIterator implements Iterator<T> {
 
         private int index = 0;
+        private boolean atStart = true;
 
         @Override
         public boolean hasNext() {
-            return (index != (tailIndex % queue.length));
+            if (RingBuffer.this.isEmpty()) {
+                return false;
+            }
+            return atStart || ((index%queue.length) != (tailIndex % queue.length));
         }
 
         @Override
         public T next() {
+            atStart = false;
             try {
-                T next = queue[index];
+                T next = queue[(index+(headIndex%queue.length))%queue.length];
                 index++;
                 return next;
             } catch (ArrayIndexOutOfBoundsException a) {
@@ -217,11 +261,11 @@ public class RingBuffer<T> implements BlockingQueue<T>{
         @Override
         public void remove() {
             try {
-                queue[index-1] = null;
+                System.arraycopy(queue, index, queue, index - 1, queue.length - index);
+                tailIndex--;
             } catch (ArrayIndexOutOfBoundsException a) {
                 throw new IllegalStateException();
             }
         }
-
     }
 }
