0f1288bcfd37548b16ed0e03d06483e87a29e61f
Eileen Liu <contact.eileenliu@gmail.com>
2012-11-28 05:04:37 -0500

    added Permission.java Signed-off-by: Eileen Liu <contact.eileenliu@gmail.com>

---
+++ student/Permission.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+/**
+ *
+ * @author Eileen Liu <el544@cornell.edu>
+ */
+public enum Permission {
+    USER,
+    ADMIN;
+}
